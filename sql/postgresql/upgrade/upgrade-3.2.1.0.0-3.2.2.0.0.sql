52532f4ac836b3d103853a542b1df5209eee7494
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-05-02 11:27:59 +0000

    - fixing update error PO-3.1.4 - 3.3 on Windows Installer

--- sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
@@ -3,6 +3,23 @@
 SELECT acs_log__debug('/packages/intranet-exchange-rate/sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql','');
 
 
+create or replace function im_day_enumerator (
+	date, date
+) returns setof date as '
+declare
+	p_start_date		alias for $1;
+	p_end_date		alias for $2;
+	v_date			date;
+BEGIN
+	v_date := p_start_date;
+	WHILE (v_date < p_end_date) LOOP
+		RETURN NEXT v_date;
+		v_date := v_date + 1;
+	END LOOP;
+	RETURN;
+end;' language 'plpgsql';
+
+
 create or replace function inline_0 ()
 returns integer as '
 declare
